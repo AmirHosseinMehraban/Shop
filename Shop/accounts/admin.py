fa66af33250d00448cd1d05c6545b396d627887b
amir_hm <ahm.mehrabanamirh@gmail.com>
2023-12-14 23:00:10 +0330

    add manager.py and form.py and Changing to a custom user model

--- Shop/accounts/admin.py
+++ Shop/accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from .models import MyUser
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ['full_name', 'is_admin']
+    list_filter = ['is_admin']
+    fieldsets = [
+        (None, {"fields": ['phone_number', 'password']}),
+        ("Personal info", {'fields': ['full_name', 'email']}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["phone_number", "email", "fullname", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email", "phone_number"]
+    ordering = ["full_name"]
+    filter_horizontal = []
+
+
+admin.site.register(MyUser, UserAdmin)
+
+admin.site.unregister(Group)
--- Shop/accounts/models.py
+++ Shop/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from .manager import MyUserManager
 
-# Create your models here.
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField( max_length=255, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["email", "full_name"]
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
