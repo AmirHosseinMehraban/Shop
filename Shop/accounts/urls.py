6dbcfdffa386868f88a878035968d66a0205b86a
amir_hm <ahm.mehrabanamirh@gmail.com>
2023-12-15 18:25:54 +0330

    add accounts.urls,  UserRegisterSerializer in serializers, Register in view

---
+++ Shop/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+app_name = 'accounts'
+urlpatterns = [
+    path('', views.Register.as_view(), name="Register"),
+]
--- Shop/accounts/views.py
+++ Shop/accounts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class Register(CreateAPIView):
+    def get(self, request):
+        return Response({"hello"})
+    def post(self, request):
+        srz_data = UserRegisterSerializer(data=request.data)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response({"message": "user create successfully"}, status=status.HTTP_201_CREATED)
+        return Response(srz_data.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+
