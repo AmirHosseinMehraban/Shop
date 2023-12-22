8844dc0d343b1401a0b04a72038b09024aa37ae8
Amir_hm <ahm.mehrabanamir@gmail.com>
2023-12-22 16:33:34 +0330

    create Category and Product model in home.models

--- Shop/home/models.py
+++ Shop/home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=50)
+    is_sub = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ManyToManyField(Category, related_name='pcategory')
+    slug = models.SlugField(max_length=50)
+    image = models.ImageField(null=True, blank=True)
+    description = models.TextField()
+    price = models.FloatField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
