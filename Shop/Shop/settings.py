97d3904ca3c6fbe56e4889781858e487142f4def
Amir_hm <ahm.mehrabanamir@gmail.com>
2023-12-22 16:19:44 +0330

    create home app

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'home.apps.HomeConfig',
     'rest_framework',
     'rest_framework.authtoken',
 ]
