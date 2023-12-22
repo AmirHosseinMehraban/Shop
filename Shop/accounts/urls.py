185136c55cd631a97e57eda469f9f0b1e4ffd69f
Amir_hm <ahm.mehrabanamir@gmail.com>
2023-12-22 16:12:18 +0330

    sending and testing email.

--- Shop/accounts/urls.py
+++ Shop/accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.Register.as_view(), name="Register"),
     path('login/', views.login, name="Login"),
     path('logout/', views.Logout.as_view(), name="Logout"),
+    path("kkk/", views.Test.as_view())
 
 
 ]
--- Shop/accounts/views.py
+++ Shop/accounts/views.py
@@ -11,11 +11,14 @@ login = obtain_auth_token
 
 class Register(CreateAPIView):
     def get(self, request):
+        print("*"*99)
+        request.session['hello']='foo'
         return Response({"hello"})
     def post(self, request):
         srz_data = UserRegisterSerializer(data=request.data)
         if srz_data.is_valid():
-            srz_data.save()
+            # srz_data.save()
+            request.session['verifycode'] = {'srz_data': srz_data, 'code': 1234}
             return Response({"message": "user create successfully"}, status=status.HTTP_201_CREATED)
         return Response(srz_data.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
 
@@ -25,6 +28,13 @@ class Logout(APIView):
     def post(self, request):
         request.user.auth_token.delete()
         return Response({"message": "user logout"}, status=status.HTTP_204_NO_CONTENT)
+
+
+class Test(APIView):
+    def get(self, request):
+        print("#"*99)
+        print(request.session['hello'])
+        return Response({"hello"})
 
 
 
